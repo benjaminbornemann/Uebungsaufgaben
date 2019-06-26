ebc51b732633f678601812a59f83e5c35a6f3dca
Bornemann <Bornemann@PCBORNEMANN.comline.de>
2019-06-26 17:23:03 +0200

    20190626

---
+++ Uebungsaufgaben/src/T2/W_2_3.java
@@ -0,0 +1,14 @@
+package T2;
+
+import javafx.scene.shape.Circle;
+
+public class W_2_3 {
+	public static void main (String[] args) {
+		Circle kreis = new Circle();
+		kreis.setCenterX(50);
+		kreis.setCenterY(50);
+		kreis.setRadius(30);
+		System.out.println(kreis);
+	}
+			
+}
